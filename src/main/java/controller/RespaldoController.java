d362990d091a22e962df435cb70bddf6fdf6f106
Esteban Baez <3st3b4nbaez@gmail.com>
2021-08-22 13:32:36 -0600

    Creacion de RespaldoController
    Creacion de Respaldo Controller

---
+++ src/main/java/controller/RespaldoController.java
@@ -0,0 +1,20 @@
+
+package controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+
+@Named(value = "repaldoController")
+@SessionScoped
+public class RespaldoController implements Serializable {
+    
+    
+
+    public RespaldoController() {
+    }
+    
+    
+    
+}
